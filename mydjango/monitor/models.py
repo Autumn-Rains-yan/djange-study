e1e930aa8c83c3d9b3f9caf9ccf9c3586d0c0c1c
wujc <237977800>
2020-03-11 10:38:39 +0800

    学习提交

--- mydjango/monitor/models.py
+++ mydjango/monitor/models.py
@@ -9,3 +9,4 @@ class UserInfo(models.Model):
     password = models.CharField(max_length=64)
     age = models.CharField(max_length=3, blank=True, null=True)
     gender = models.CharField(max_length=16, blank=True, null=True)
+    email = models.EmailField(max_length=32, blank=True, null=True)
